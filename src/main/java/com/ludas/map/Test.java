d68645f972a97f7196addb7a2ee8d333221a1b7b
qilin.huang <qilin.huang@fengjr.com>
2019-02-26 15:28:38 +0800

    first

---
+++ src/main/java/com/ludas/map/Test.java
@@ -0,0 +1,27 @@
+package com.ludas.map;
+
+public class Test {
+
+    /**
+     * 面试题：为啥HashMap的长度是2^n
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        /**
+         * map中计算 key在数组中的index位置
+         * 算法：index=hash&(length-1) == hash%length
+         *
+         * 解答：只有length是2^n的时候hash&(length-1) == hash%length
+         */
+
+        int length = 16;
+        int hash = 123121;
+
+        int i = hash % length;
+        System.out.println("%=" + i);
+        int j = hash & (length - 1);
+        System.out.println("&=" + j);
+
+    }
+}
