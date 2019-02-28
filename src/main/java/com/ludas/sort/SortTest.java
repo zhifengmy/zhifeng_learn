1cd910791d82d5e2d36e69561367a9db51ca8cea
qilin.huang <qilin.huang@fengjr.com>
2019-02-28 10:32:48 +0800

    first

--- src/main/java/com/ludas/sort/SortTest.java
+++ src/main/java/com/ludas/sort/SortTest.java
@@ -14,19 +14,19 @@ public class SortTest {
 
         int[] array = {4, 2, 5, 3, 6, 0};
         /**
-         * 插入排序 原地，稳定算法
+         * 选择排序
          */
         int length=array.length;
         for(int i=0;i<length;i++){
             int value=array[i];
-            for(int j=0;i<i;j++){
-                if(value<array[j]){
-                   //数据移动
-
-                }else{
+            for(int j=i;j<length;j++){
+                 //找最小的位置
+                if(value>array[j]){
 
                 }
             }
+
+
         }
 
         System.out.println(Arrays.toString(array));
