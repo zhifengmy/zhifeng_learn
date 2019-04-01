9d8e70f677ba8ccdc086d525c1b267544a5fe0e2
qilin.huang <qilin.huang@fengjr.com>
2019-04-01 14:22:53 +0800

    canal

---
+++ src/main/java/com/ludas/canal/CanalClientTest.java
@@ -0,0 +1,16 @@
+package com.ludas.canal;
+
+
+/**
+* @Description:canal客户端测试
+* @author KyLin
+* @date 2019/4/1 14:15
+*/
+public class CanalClientTest {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
