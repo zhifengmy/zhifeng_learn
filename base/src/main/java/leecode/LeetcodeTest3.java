431f5b1d3a4274fc1b7ee47398f67adba66fbd88
qilin.huang <qilin.huang@fengjr.com>
2019-04-15 16:44:24 +0800

    整理工程结构

---
+++ base/src/main/java/leecode/LeetcodeTest3.java
@@ -0,0 +1,25 @@
+package leecode;
+
+import java.util.List;
+
+/**
+ * @author LuDas
+ * @Description:给定一个字符串，请你找出其中不含有重复字符的最长子串的长度。
+ * @date 2019/4/10 15:31
+ */
+public class LeetcodeTest3 {
+
+    /**
+     * 输入: "abcabcbb"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+     */
+
+
+    public static void main(String[] args) {
+       int a=1;
+        do {
+            System.out.println(1111111);
+        } while (a!=1);
+    }
+}
