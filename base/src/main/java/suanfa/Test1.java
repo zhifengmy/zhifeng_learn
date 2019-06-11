2579112bc033affd7a5cc13efaa4d7d470e8740f
qilin.huang <hql@!2027>
2019-06-11 16:41:01 +0800

    练习

---
+++ base/src/main/java/suanfa/Test1.java
@@ -0,0 +1,42 @@
+package suanfa;
+
+
+/**
+ * 有序List求交集
+ * {2,3,6,8,12}
+ * {1,4,6,7,8}
+ */
+public class Test1 {
+
+    /**
+     * 1、双重for循环
+     * 2、拉链法
+     *
+     *
+     */
+
+    public static void main(String[] args) {
+
+        int[] list1={2,3,6,8,12};
+        int[] list2={1,4,6,7,8};
+
+
+    }
+
+    /**
+     * 拉链法
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static int[] listNode(int[] l1,int[] l2){
+
+
+
+
+        return null;
+    }
+
+
+
+}
