8e51145103c63b2b3544de812d1376ac7a096b6e
qilin.huang <qilin.huang@fengjr.com>
2020-07-01 00:14:13 +0800

    add eventbus

--- base/src/main/java/io/ReadBigFileTest.java
+++ base/src/main/java/io/ReadBigFileTest.java
@@ -1,6 +1,7 @@
 package io;
 
 import java.io.*;
+import java.util.*;
 
 /**
  * Created with 栉风
@@ -11,55 +12,61 @@ public class ReadBigFileTest {
 
 
     public static void main(String[] args) {
+        //分隔大文件
+        splitBigFile();
+        File file = new File("/home");
+        for (File f : file.listFiles()) {
+            print(f);
+        }
 
     }
 
     /**
      * Java读取超大文件
-     *
+     * <p>
      * 文件5G,空格分隔
-     * 格式：time            id url
-     * 2019-09-01 09:00:00 123 http://www.kuaishou.com
+     * 格式：
+     * time                  id       url
+     * 2019-09-01 09:00:00   123      http://www.kuaishou.com
      * id 范围 50亿，Long类型
-     *
+     * <p>
      * 要求：解析文件，统计并打印每个用户的总访问次数，最终打印出用户id和总访问次数
-     *
+     * <p>
+     * Linux awk '{print $1}' log.txt|sort|uniq -c | sort -nr
      */
-    public static void splitBigFile(){
-        BufferedInputStream fis=null;
-        BufferedReader reader=null;
+    public static void splitBigFile() {
+        BufferedInputStream fis = null;
+        BufferedReader reader = null;
         FileWriter fw = null;
-
         try {
-            File file = new File("");
-            fw = new FileWriter("");
+            File file = new File("/Users/hql/log.txt");
             fis = new BufferedInputStream(new FileInputStream(file));
-            reader = new BufferedReader(new InputStreamReader(fis,"utf-8"),1024*1024*1024);// 用1G的缓冲读取文本文件
+            reader = new BufferedReader(new InputStreamReader(fis, "utf-8"), 1024);// 用1G的缓冲读取文本文件
 
             String line = "";
-            while((line = reader.readLine()) != null){
+            while ((line = reader.readLine()) != null) {
                 String[] split = line.split(" ");
-                String time=split[0];
-                String idStr=split[1];
-                String url=split[2];
+                String idStr = split[1];
 
-                Long id = Long.valueOf(idStr);
-                if(id>0&&id <1000000000){
-                    fw.append(line + " ");
-                }else if(id>1000000000&&id <2000000000){
-                    fw.append(line + " ");
-                }
-
-
-
+                int i = idStr.hashCode() % 1000; //0-999
+                fw = new FileWriter("/Users/hql/file_" + i + ".txt", true);
+                fw.append(line + System.lineSeparator());
+                fw.flush();
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
-                fis.close();
-                reader.close();
-            }catch (Exception e){
+                if (reader != null) {
+                    reader.close();
+                }
+                if (fis != null) {
+                    fis.close();
+                }
+                if (fw != null) {
+                    fw.close();
+                }
+            } catch (Exception e) {
                 e.printStackTrace();
             }
 
@@ -68,7 +75,55 @@ public class ReadBigFileTest {
     }
 
 
+    public static void print(File file) {
 
+        if (file == null) {
+            return;
+        }
+
+        BufferedInputStream fis = null;
+        BufferedReader reader = null;
+        FileWriter fw = null;
+        try {
+            fis = new BufferedInputStream(new FileInputStream(file));
+            reader = new BufferedReader(new InputStreamReader(fis, "utf-8"), 1024);// 用1G的缓冲读取文本文件
+            Map<String, Long> countMap = new TreeMap<>();
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                String[] split = line.split(" ");
+                String id = split[1];
+                if (countMap.containsKey(id)) {
+                    countMap.put(id, countMap.get(id) + 1L);
+                } else {
+                    countMap.put(id, 1L);
+                }
+                //遍历
+                for (Map.Entry<String, Long> entry : countMap.entrySet()) {
+                    String key = entry.getKey();
+                    Long value = entry.getValue();
+                    System.out.println(key + ">>>>>>>" + value);
+                }
+            }
+        } catch (Exception e) {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+                if (fis != null) {
+                    fis.close();
+                }
+                if (fw != null) {
+                    fw.close();
+                }
+            } catch (Exception se) {
+                se.printStackTrace();
+            }
+
+        }
+
+
+    }
 
 
 }
